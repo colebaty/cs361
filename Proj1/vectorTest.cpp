dce13e6b73fd23fe1edc90b772baf91ba78a58e8
Cole <tbaty002@odu.edu>
2022-02-03 20:19:26 -0500

    container class tested; tested different collections, might go with vector of vectors of stacks (top-down)

--- Proj1/vectorTest.cpp
+++ Proj1/vectorTest.cpp
@@ -15,6 +15,7 @@
 #include <cstdlib>
 #include <queue>
 #include <vector>
+#include <stack>
 
 #include "container.h"
 
@@ -41,12 +42,75 @@ int main(int argc, char** argv)
     assert(row.size() == 10);
     cout << "pass" << endl;
     cout << "displaying contents of container row" << endl;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < row.size(); i++)
     {
         row[i].display();
         cout << endl;
     }
 
+    cout << "--------------------------" << endl;
+
+    cout << "creating a slice/stack of 10 rows" << endl;
+    stack<vector<container>> slice;
+    vector<container> * rowptr;
+
+    int id = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        rowptr = new vector<container>;
+        for (int j = 0; j < 10; j++)
+        {
+            cont = new container(id++);
+            rowptr->push_back(*cont);
+        }
+        slice.push(*rowptr);
+    }
+
+    while (!slice.empty())
+    {
+        row = slice.top();
+        for (int i = 0; i < row.size(); i++)
+        {
+            row[i].display();
+            cout << endl;
+        }
+
+        slice.pop();
+        cout << "-------------------------" << endl;
+    }
     
+    cout << "--------------end of slice" << endl;
+    
+    // do columns of stacks?
+    cout << "---------------------" << endl
+         << "creating layers of columns of stacks" << endl;
+
+    stack<container> * colptr;
+
+    vector<stack<container>> layer;
+
+    id = 100;
+    for (int i = 0; i < 5; i++)
+    {
+        colptr = new stack<container>;
+        for (int j = 0; j < 5; j++)
+        {
+           cont = new container(id++);
+           colptr->push(*cont); 
+        }
+        layer.push_back(*colptr);
+    }
+
+    for (int i = 0; i < layer.size(); i++)
+    {
+        while(!layer[i].empty())
+        {
+            layer[i].top().display();
+            cout << endl;
+            layer[i].pop();
+        }
+        cout << "----------------------" << endl;
+    }
+
     return 0;
 }

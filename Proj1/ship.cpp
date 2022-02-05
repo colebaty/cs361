2fc349d77e3d9de4e3348dd37192912b0cd1c0f6
Cole <tbaty002@odu.edu>
2022-02-05 16:13:19 -0500

    ship class started; beginning testing

---
+++ Proj1/ship.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include "shiputils.h"
+#include "ship.h"
+#include "container.h"
+
+using namespace std;
+
+ship::ship()
+{
+    _id = 0;
+    _capacity = 0;
+    _cargo = nullptr;
+    _contsPerLevel = 0;
+    _width = 0;
+    _length = 0;
+}
+
+ship::ship(int id, int capacity)
+{
+    _id = id;
+    _capacity = capacity;
+    *_cargo = fillCargo();
+}
+
+vector<vector<stack<container>>> ship::fillCargo()
+{
+    _contsPerLevel = _capacity * 0.5;
+
+    //round up to nearest multiple of 5
+    _contsPerLevel += _capacity % 5;
+    _width = _contsPerLevel * 0.1;
+    _length = _contsPerLevel - _width;
+}
+
+void ship::display()
+{
+    cout << "ship:\t\t" << _id << endl
+         << "capacity\t" << _capacity << endl
+         << "containers/level:\t" << _contsPerLevel << endl
+         << "width:\t\t" << _width << endl
+         << "length:\t\t" << _length << endl;
+
+}
---
+++ Proj1/ship.h
@@ -0,0 +1,60 @@
+#ifndef __SHIP_H__
+#define __SHIP_H__
+
+#include <vector>
+#include <stack>
+
+#include "shiputils.h"
+#include "container.h"
+
+using namespace std;
+
+const int MAX_CAPACITY = 1000;
+const int MIN_CAPACITY = 750;
+
+class ship {
+    private:
+        int _id;
+        int _capacity;
+        
+        //super private instance variables just for debugging
+        int _contsPerLevel;
+        int _width;
+        int _length;
+
+        /**
+         * @brief a 2D vector of stacks of containers. a 2D vector of stacks 
+         * eliminates the need to access any container below the topmost 
+         */
+        vector<vector<stack<container>>> * _cargo;
+        //private methods
+
+        /**
+         * @brief private method to fill the cargo hold. containers are
+         * generated with incrementing IDs and pseud-randomly generated 
+         * destination codes
+         */
+        vector<vector<stack<container>>> fillCargo();
+    public:
+        /**
+         * @brief default constructor, initializes a ship object with
+         * id = 0, capacity = 0;
+         * 
+         */
+        ship();
+
+        /**
+         * @brief initializes a ship object with the specified ID. unless otherwise 
+         * specified, capacity will be a random integer on the interval 
+         * [MIN_CAPACITY, MIN_CAPACITY].
+         * 
+         * @param id 
+         * @param min 
+         * @param max 
+         */
+        ship(int id, int capacity = getRand(MIN_CAPACITY, MAX_CAPACITY));
+
+        void display();
+};
+
+#endif //__SHIP_H__

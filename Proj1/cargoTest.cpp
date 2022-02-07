6774df3c7a2e02cb54d8f0045f6a4bfef3689d22
Cole <tbaty002@odu.edu>
2022-02-06 21:43:57 -0500

    filling container tested in cargoTest; implementing fillCargo()

--- Proj1/cargoTest.cpp
+++ Proj1/cargoTest.cpp
@@ -15,51 +15,26 @@ int main()
     int contCount = 0;
 
     container * contptr;
+    stack<container> * column;
+    vector<stack<container>> * layer;
+    vector<vector<stack<container>>> cargo;
 
-
-    //make three stacks
-    stack<container> * stackptr1 = new stack<container>;
-    stack<container> * stackptr2 = new stack<container>;
-    stack<container> * stackptr3 = new stack<container>;
-
-    //make three containers
-    for (int i = 0; i < 3; i++)
+    for (int row = 0; row < 3; row++)
     {
-        //make new container
-        contptr = new container(contID++);
-        contCount++;
-        //put container in stack1
-        stackptr1->push(*contptr);
+        layer = new vector<stack<container>>;
+        for (int col = 0; col < 3; col++)
+        {
+            column = new stack<container>;
+            for (int i = 0; i < 3; i++)
+            {
+                contptr = new container(contID++);
+                contCount++;
+                column->push(*contptr);
+            }
+            layer->push_back(*column);
+        }
+        cargo.push_back(*layer);
     }
-    assert(stackptr1->size() == 3);
-
-    //make three more containers
-    for (int i = 0; i < 3; i++)
-    {
-        //make new container
-        contptr = new container(contID++);
-        contCount++;
-        //put container in stack2
-        stackptr2->push(*contptr);
-    }
-    assert(stackptr2->size() == 3);
-
-    //make three more containers
-    for (int i = 0; i < 3; i++)
-    {
-        //make new container
-        contptr = new container(contID++);
-        contCount++;
-        //put container in stack3
-        stackptr3->push(*contptr);
-    }
-    assert(stackptr3->size() == 3);
-
-    //put stacks in column vector
-    vector<stack<container>> * colptr = new vector<stack<container>>;
-    colptr->push_back(*stackptr1);
-    colptr->push_back(*stackptr2);
-    colptr->push_back(*stackptr3);
 
     std::cout << contCount << " containers generated" << endl;
     std::cout << "------------------------" << endl;

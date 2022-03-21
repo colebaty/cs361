63a4bdb5bf352cf3331a2dbf87811b8da4b5e5a2
cs_tbaty002 <tbaty002@.odu.edu>
2022-03-21 12:00:14 -0400

    housekeeping

--- Proj2/utilities.h
+++ Proj2/utilities.h
@@ -10,6 +10,7 @@ using namespace std;
 
 const string DATA_FILE_EXT = ".dat";
 const string COMMAND_FILE_EXT = ".gpc"; //gnuplot command
+const string IMAGE_FILE_EXT = ".png";
 
 void writeData(module mod);
 

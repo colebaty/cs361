79e563f13eb9396514e5dc1c9fb5adb44e9bfc6d
Cole <cole.baty@gmail.com>
2022-03-23 18:38:50 -0400

    tested whether stream objects preserve state through function calls

---
+++ Proj2/foutTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void first(ofstream& out);
+void second(ofstream& out);
+
+int main()
+{
+    cout << "testing whether state is preserved when fstream"
+         << "object is passed as param through multiple functions" << endl;
+
+    cout << "------------------------------------------" << endl;
+    cout << "creating new ofstream object to write to 'out.dat'" << endl;
+
+    ofstream * fout = new ofstream("out.dat");
+    *fout << "from main" << endl;
+    first(*fout);
+    fout->close();
+
+    //housekeeping
+    delete fout;
+
+    return 0;
+}
+
+void first(ofstream& out)
+{
+    out << "\tfrom first" << endl;
+    second(out);
+}
+void second(ofstream& out)
+{
+    out << "\t\tfrom second" << endl;
+}
